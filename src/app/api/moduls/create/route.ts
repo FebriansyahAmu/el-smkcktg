faa79db1ae2102a70ad79dbc860e66531469353a
Febriansyah <febriansyah.amu1@gmail.com>
2025-04-11 21:43:18 +0800

    add: modul api, error handling, validation input

--- src/app/api/moduls/create/route.ts
+++ src/app/api/moduls/create/route.ts
@@ -2,3 +2,57 @@ import { NextResponse, NextRequest } from "next/server";
 import { getSession } from "@/app/lib/session";
 import { createModul } from "@/app/lib/data-access/moduldal";
 import { validateModulInput } from "@/app/lib/validation/modulValidation";
+import { ValidationError } from "yup";
+
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getSession(request);
+    if (!session || session.role != "Guru") {
+      return NextResponse.json(
+        {
+          status: "error",
+        },
+        { status: 401 }
+      );
+    }
+
+    const body = await request.json();
+
+    //validasi input
+    await validateModulInput(body);
+
+    //fire up! get sucked into dtabase
+    await createModul(body);
+
+    return NextResponse.json(
+      {
+        status: "success",
+        message: "Kelas berhasil dibuat.",
+      },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    if (error instanceof ValidationError) {
+      const formattedErrors = error.inner.map((e) => ({
+        field: e.path,
+        message: e.message,
+      }));
+
+      return NextResponse.json(
+        {
+          status: "gagal",
+          errors: formattedErrors,
+        },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        status: "error",
+        message: "Terjadi kesalahan pada server, silahkan coba lagi nanti",
+      },
+      { status: 500 }
+    );
+  }
+}
--- src/app/lib/validation/modulValidation.ts
+++ src/app/lib/validation/modulValidation.ts
@@ -3,6 +3,7 @@ import * as yup from "yup";
 export const createModulSchema = yup.object({
   title: yup.string().required().min(3),
   description: yup.string().required().min(5),
+  id_course: yup.number().required(),
 });
 
 export type CreateModulInput = yup.InferType<typeof createModulSchema>;
