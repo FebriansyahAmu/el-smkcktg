1dfccffaaab0aa3ff05bb222d7ea9965755e2652
Febriansyah <febriansyah.amu1@gmail.com>
2025-04-11 12:45:05 +0800

    add: fix validation modul

--- src/app/lib/validation/modulValidation.ts
+++ src/app/lib/validation/modulValidation.ts
@@ -4,3 +4,9 @@ export const createModulSchema = yup.object({
   title: yup.string().required().min(3),
   description: yup.string().required().min(5),
 });
+
+export type CreateModulInput = yup.InferType<typeof createModulSchema>;
+
+export async function validateModulInput(input: CreateModulInput) {
+  return await createModulSchema.validate(input, { abortEarly: false });
+}
